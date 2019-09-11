4455bde90a4c6d485a7b854d734dcec27951b150
nuttadet2527 <53985212+nuttadet2527@users.noreply.github.com>
2019-09-11 12:00:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,185 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
 <?php header( 'Location: /index.html' ) ;  ?>
+{
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "carousel",
+    "contents": [
+      {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "url": "https://www.ingredientfarm.com/Images/Products/Large/2156172-kenta pouch 5.jpg",
+          "size": "full",
+          "aspectRatio": "20:13",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "text",
+              "text": "เคนตะ คอลลาเจน",
+              "size": "xl",
+              "weight": "bold",
+              "wrap": true
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "590",
+                  "flex": 0,
+                  "size": "xl",
+                  "weight": "bold",
+                  "wrap": true
+                },
+                {
+                  "type": "text",
+                  "text": "บาท",
+                  "flex": 0,
+                  "size": "sm",
+                  "weight": "bold",
+                  "wrap": true
+                }
+              ]
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "message",
+                "label": "สั่งซื้อเซ็ดนี้",
+                "text": "ซื้อเลย"
+              },
+              "flex": 1,
+              "style": "primary"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "message",
+                "label": "ชอบ",
+                "text": "ดีมาก"
+              },
+              "color": "#FFFFFF"
+            }
+          ]
+        }
+      },
+      {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "url": "https://www.ingredientfarm.com/Images/Products/Large/7648655-kenta pouch (1).jpg",
+          "size": "full",
+          "aspectRatio": "20:13",
+          "aspectMode": "cover"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "text",
+              "text": "คอลลาเจน 3 ซอง",
+              "size": "xl",
+              "weight": "bold",
+              "wrap": true
+            },
+            {
+              "type": "box",
+              "layout": "baseline",
+              "flex": 1,
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "1170",
+                  "flex": 0,
+                  "size": "xl",
+                  "weight": "bold",
+                  "wrap": true
+                },
+                {
+                  "type": "text",
+                  "text": ".99",
+                  "flex": 0,
+                  "size": "sm",
+                  "weight": "bold",
+                  "wrap": true
+                }
+              ]
+            },
+            {
+              "type": "text",
+              "text": "Temporarily out of stock",
+              "flex": 0,
+              "margin": "md",
+              "size": "xxs",
+              "color": "#FF5551",
+              "wrap": true
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Add to Cart",
+                "uri": "https://linecorp.com"
+              },
+              "flex": 2,
+              "color": "#AAAAAA",
+              "style": "primary"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Add to wish list",
+                "uri": "https://linecorp.com"
+              }
+            }
+          ]
+        }
+      },
+      {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "See more",
+                "uri": "https://linecorp.com"
+              },
+              "flex": 1,
+              "gravity": "center"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
